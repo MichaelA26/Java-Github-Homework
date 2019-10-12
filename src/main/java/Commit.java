1fd4dd45d2bb53bbe9b2503f86306a8ea53c051a
Michael Alexander <michael_alexander_20@outlook.com>
2019-10-12 14:43:27 +0100

    add description, commitype, and uniqueid to commit

--- src/main/java/Commit.java
+++ src/main/java/Commit.java
@@ -1,5 +1,13 @@
 public class Commit {
 
+    private Description description;
+    private CommitType committype;
+    private UniqueID uniqueid;
 
+    public Commit(CommitType committype, Description description, UniqueID uniqueid) {
+        this.description = description;
+        this.committype = committype;
+        this.uniqueid = uniqueid;
+    }
 
 }
--- src/test/java/CommitTest.java
+++ src/test/java/CommitTest.java
@@ -1,5 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
 public class CommitTest {
 
+    Commit commit;
 
-    
+    @Before
+    public void setup(){
+        commit = new Commit(CommitType.FEATURE);
+    }
+
+//    @Test
+//    public void
 }
